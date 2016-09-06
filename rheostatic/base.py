5184f2821082e1809bb928036b8c3c2ae64f3d12
Waylan Limberg <waylan.limberg@icloud.com>
2016-09-06 00:23:18 -0400

    Working but incomplete code

---
+++ rheostatic/base.py
@@ -0,0 +1,208 @@
+"""
+Rheostatic - A Static File Server with options.
+
+MIT License
+
+Copyright (c) 2016 Waylan Limberg
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+import os
+import sys
+import posixpath
+import urllib
+import urlparse
+import cgi
+import wsgiref
+from email import utils as rfc822
+try:
+    from cStringIO import StringIO
+except ImportError:
+    from StringIO import StringIO
+
+from . import utils
+
+
+class Rheostatic(object):
+    """
+    Static File Server with options.
+
+    Serve static files from the given root directory and any subdirectories.
+    Responds to GET and HEAD requests.
+
+    """
+
+    server_version = 'rheostatic/' + utils.__version__
+    index_file = 'index.html'
+    default_extension = None
+    default_type = 'application/octet-stream'
+    encoding = 'utf-8'
+    directory_template = utils.directory_template
+
+    def __init__(self, root, **kwargs):
+        self.root = os.path.abspath(root)
+
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def __call__(self, environ, start_response):
+        """ Send the response code and MIME headers. """
+        if environ['REQUEST_METHOD'] not in ('GET', 'HEAD'):
+            # Unsupported method
+            headers = [('Allow', 'GET, HEAD')]
+            return self.error(405, environ, start_response, headers)
+
+        path_info = environ.get('PATH_INFO', '')
+        path = self.get_full_path(path_info)
+        print 'PATH_INFO:', path_info, 'LOCAL_PATH:', path
+        if not path.startswith(self.root):
+            # Outside server root
+            return self.error(404, environ, start_response)
+
+        if os.path.isdir(path):
+            if not path_info.endswith('/'):
+                # Dir does not end with /, redirect
+                location = wsgiref.util.request_uri(environ, include_query=False) + '/'
+                if environ.get('QUERY_STRING'):
+                    location += '?' + environ.get('QUERY_STRING')
+                headers = [('Location', location)]
+                return self.simple_error(301, environ, start_response, headers)
+            index = os.path.join(path, self.index_file)
+            if os.path.isfile(index):
+                path = index
+            else:
+                return self.list_directory(path, environ, start_response)
+
+        if os.path.isfile(path):
+            try:
+                file_stat = os.stat(path)
+                headers = [
+                    ('Date', rfc822.formatdate(usegmt=True)),
+                    ('Last-Modified', rfc822.formatdate(file_stat.st_mtime, usegmt=True)),
+                    ('Content-Length', str(file_stat.st_size)),
+                    ('Content-type', self.guess_type(path))  # TODO: add charset
+                ]
+                # TODO: add support for HTTP_IF_MODIFIED_SINCE and HTTP_IF_NONE_MATCH
+                start_response(self.get_status(200), headers)
+                return self.get_body(path, environ)
+            except (IOError, OSError):
+                return self.error(404, environ, start_response)
+
+        return self.error(404, environ, start_response)
+
+    def get_full_path(self, path_info):
+        """ Get local filename path from path_info. """
+        path_info = utils.decode_path_info(path_info)
+        path_info = posixpath.normpath(urllib.unquote(path_info))
+        path = os.path.normpath(self.root + path_info)
+        if (self.default_extension and
+                not os.path.exists(path) and
+                os.path.splitext(path)[1] == '' and
+                os.path.isfile(path + self.default_extension)):
+            path += self.default_extension
+        return path
+
+    def get_status(self, code):
+        return '%d %s' % (code, utils.http_status[code])
+
+    def get_body(self, path, environ):
+        if environ['REQUEST_METHOD'] == 'HEAD':
+            return [b'']
+        else:
+            file_wrapper = environ.get('wsgi.file_wrapper', wsgiref.util.FileWrapper)
+            return file_wrapper(open(path, 'rb'))
+
+    def guess_type(self, path):
+        extension = os.path.splitext(path)[1].lower()
+        return utils.types_map.get(extension, self.default_type)
+
+    def error(self, code, environ, start_response, headers=None):
+        """
+        Send an error reply.
+
+        If an error page exists at the server root in the format [code].html,
+        then that page is returned instead of the message.
+
+        """
+        headers = headers or []
+        path = os.path.join(self.root, '%d.html' % code)
+        if os.path.isfile(path):
+            try:
+                file_stat = os.stat(path)
+                headers.extend([
+                    ('Content-Length', str(file_stat.st_size)),
+                    ('Content-type', self.guess_type(path))  # TODO: add charset
+                ])
+                start_response(self.get_status(code), headers)
+                return self.get_body(path, environ)
+            except (IOError, OSError):
+                self.simple_error(code, environ, start_response, headers)
+        else:
+            self.simple_error(code, environ, start_response, headers)
+
+    def simple_error(self, code, environ, start_response, headers=None):
+        """ Send a plain text error. """
+        headers = headers or []
+        status = self.get_status(code)
+        headers.extend([
+            ('Content-Length', str(len(status))),
+            ('Content-type', 'text/plain')  # TODO: add charset
+        ])
+        start_response(status, headers)
+        return [status]
+
+    def list_directory(self, path, environ, start_response):
+        """ Return a directory listing. """
+        try:
+            names = os.listdir(path)
+        except os.error:
+            return self.error(404, environ, start_response)
+        names.sort(key=lambda a: a.lower())
+
+        items = []
+        for name in names:
+            fullname = os.path.join(path, name)
+            displayname = linkname = name
+            # Append / for directories or @ for symbolic links
+            if os.path.isdir(fullname):
+                displayname = name + "/"
+                linkname = name + "/"
+            if os.path.islink(fullname):
+                displayname = name + "@"
+                # Note: a link to a directory displays with @ and links with /
+            items.append('<li><a href="{}">{}</a></li>'.format(
+                urllib.quote(linkname), cgi.escape(displayname)
+            ))
+
+        f = StringIO()
+        f.write(self.directory_template.format(
+            displaypath = cgi.escape(urllib.unquote(wsgiref.util.request_uri(environ))),
+            items = '\n'.join(items)
+        ))
+        length = f.tell()
+        f.seek(0)
+
+        headers = [
+            ('Content-Length', str(length)),
+            ('Content-type', 'text/html')  # TODO: add charset
+        ]
+        start_response(self.get_status(200), headers)
+        file_wrapper = environ.get('wsgi.file_wrapper', wsgiref.util.FileWrapper)
+        return file_wrapper(f)
---
+++ rheostatic/utils.py
@@ -0,0 +1,194 @@
+"""
+Rheostatic - A Static File Server with options.
+
+MIT License
+
+Copyright (c) 2016 Waylan Limberg
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+
+import sys
+
+
+# version_info should conform to PEP 386
+# (major, minor, micro, alpha/beta/rc/final, #)
+# (1, 1, 2, 'alpha', 0) => "1.1.2.dev"
+# (1, 2, 0, 'beta', 2) => "1.2b2"
+__version_info__ = (0, 0, 0, 'alpha', 0)
+
+
+def _get_version():  # pragma: no cover
+    " Returns a PEP 386-compliant version number from version_info. "
+    assert len(__version_info__) == 5
+    assert __version_info__[3] in ('alpha', 'beta', 'rc', 'final')
+
+    parts = 2 if __version_info__[2] == 0 else 3
+    main = '.'.join(map(str, __version_info__[:parts]))
+
+    sub = ''
+    if __version_info__[3] == 'alpha' and __version_info__[4] == 0:
+        # TODO: maybe append some sort of git info here??
+        sub = '.dev'
+    elif __version_info__[3] != 'final':
+        mapping = {'alpha': 'a', 'beta': 'b', 'rc': 'c'}
+        sub = mapping[__version_info__[3]] + str(__version_info__[4])
+
+    return str(main + sub)
+
+
+__version__ = _get_version()
+
+
+# Follow Django in treating URLs as UTF-8 encoded (which requires undoing the
+# implicit ISO-8859-1 decoding applied in Python 3). Strictly speaking, URLs
+# should only be ASCII anyway, but UTF-8 can be found in the wild.
+if sys.version_info[0] >= 3:
+    def decode_path_info(path_info):
+        return path_info.encode('iso-8859-1').decode('utf-8')
+else:
+    def decode_path_info(path_info):
+        return path_info.decode('utf-8')
+
+
+# Define only the HTTP status codes we actually use
+http_status = {
+    200: 'OK',
+    301: 'Moved Permanently',
+    304: 'Not Modified',
+    404: 'Not Found',
+    405: 'Method Not Allowed'
+
+}
+
+directory_template = """
+<html>
+    <head>
+        <title>Directory listing for {displaypath}</title>
+    </head>
+    <body>
+        <h2>Directory listing for {displaypath}</h2>
+        <hr>
+        <ul>
+            {items}
+        </ul>
+        <hr>
+    </body>
+</html>
+"""
+
+# Define our own types for consistency cross platform.
+# Use the types defined by nginx with a few additions.
+types_map =  {
+    '.3gp': 'video/3gpp',
+    '.3gpp': 'video/3gpp',
+    '.7z': 'application/x-7z-compressed',
+    '.ai': 'application/postscript',
+    '.asf': 'video/x-ms-asf',
+    '.asx': 'video/x-ms-asf',
+    '.atom': 'application/atom+xml',
+    '.avi': 'video/x-msvideo',
+    '.bmp': 'image/x-ms-bmp',
+    '.cco': 'application/x-cocoa',
+    '.crt': 'application/x-x509-ca-cert',
+    '.css': 'text/css',
+    '.der': 'application/x-x509-ca-cert',
+    '.doc': 'application/msword',
+    '.docx': 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+    '.ear': 'application/java-archive',
+    '.eot': 'application/vnd.ms-fontobject',
+    '.eps': 'application/postscript',
+    '.flv': 'video/x-flv',
+    '.gif': 'image/gif',
+    '.hqx': 'application/mac-binhex40',
+    '.htc': 'text/x-component',
+    '.htm': 'text/html',
+    '.html': 'text/html',
+    '.ico': 'image/x-icon',
+    '.jad': 'text/vnd.sun.j2me.app-descriptor',
+    '.jar': 'application/java-archive',
+    '.jardiff': 'application/x-java-archive-diff',
+    '.jng': 'image/x-jng',
+    '.jnlp': 'application/x-java-jnlp-file',
+    '.jpeg': 'image/jpeg',
+    '.jpg': 'image/jpeg',
+    '.js': 'application/javascript',
+    '.json': 'application/json',
+    '.kar': 'audio/midi',
+    '.kml': 'application/vnd.google-earth.kml+xml',
+    '.kmz': 'application/vnd.google-earth.kmz',
+    '.m3u8': 'application/vnd.apple.mpegurl',
+    '.m4a': 'audio/x-m4a',
+    '.m4v': 'video/x-m4v',
+    '.mid': 'audio/midi',
+    '.midi': 'audio/midi',
+    '.mml': 'text/mathml',
+    '.mng': 'video/x-mng',
+    '.mov': 'video/quicktime',
+    '.mp3': 'audio/mpeg',
+    '.mp4': 'video/mp4',
+    '.mpeg': 'video/mpeg',
+    '.mpg': 'video/mpeg',
+    '.ogg': 'audio/ogg',
+    '.pdb': 'application/x-pilot',
+    '.pdf': 'application/pdf',
+    '.pem': 'application/x-x509-ca-cert',
+    '.pl': 'application/x-perl',
+    '.pm': 'application/x-perl',
+    '.png': 'image/png',
+    '.ppt': 'application/vnd.ms-powerpoint',
+    '.pptx': 'application/vnd.openxmlformats-officedocument.presentationml.presentation',
+    '.prc': 'application/x-pilot',
+    '.ps': 'application/postscript',
+    '.ra': 'audio/x-realaudio',
+    '.rar': 'application/x-rar-compressed',
+    '.rpm': 'application/x-redhat-package-manager',
+    '.rss': 'application/rss+xml',
+    '.rtf': 'application/rtf',
+    '.run': 'application/x-makeself',
+    '.sea': 'application/x-sea',
+    '.shtml': 'text/html',
+    '.sit': 'application/x-stuffit',
+    '.svg': 'image/svg+xml',
+    '.svgz': 'image/svg+xml',
+    '.swf': 'application/x-shockwave-flash',
+    '.tcl': 'application/x-tcl',
+    '.tif': 'image/tiff',
+    '.tiff': 'image/tiff',
+    '.tk': 'application/x-tcl',
+    '.ts': 'video/mp2t',
+    '.txt': 'text/plain',
+    '.war': 'application/java-archive',
+    '.wbmp': 'image/vnd.wap.wbmp',
+    '.webm': 'video/webm',
+    '.webp': 'image/webp',
+    '.wml': 'text/vnd.wap.wml',
+    '.wmlc': 'application/vnd.wap.wmlc',
+    '.wmv': 'video/x-ms-wmv',
+    '.woff': 'application/font-woff',
+    '.woff2': 'font/woff2',
+    '.xhtml': 'application/xhtml+xml',
+    '.xls': 'application/vnd.ms-excel',
+    '.xlsx': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+    '.xml': 'text/xml',
+    '.xpi': 'application/x-xpinstall',
+    '.xspf': 'application/xspf+xml',
+    '.zip': 'application/zip'
+}
