a720c3b074c4c82d91ecd2d4ea8ff1f041c18696
Waylan Limberg <waylan.limberg@icloud.com>
2016-09-08 22:07:34 -0400

    Make a proper package

---
+++ README.rst
@@ -0,0 +1,38 @@
+==========
+Rheostatic
+==========
+
+.. default-role:: code
+
+A Static File Server with options.
+
+Installation
+------------
+
+To install Rheostatic run the following command::
+
+    pip install https://github.com/waylan/rheostatic/archive/master.zip
+
+Note that this is currently **alpha** software and not yet hosted on PyPI. As such, the
+above command downloads the source code from GitHub. Upon a stable release, the package will
+become available from PyPI.
+
+Usage
+-----
+
+TODO...
+
+Dependencies
+------------
+
+Rheostatic is a pure Python library with no external dependencies. It should run without issue
+on CPython versions 2.7, 3.3, 3.4, and 3.5 as well as `PyPy`_.
+
+.. _`PyPy`: http://pypy.org/
+
+License
+-------
+
+Rheostatic is licensed under the `MIT License`_ as defined in `LICENSE`.
+
+.. _`MIT License`: https://opensource.org/licenses/MIT
---
+++ setup.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+"""
+Rheostatic - A Static File Server with options.
+
+MIT License
+
+Copyright (c) 2016 Waylan Limberg
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+from setuptools import setup
+
+with  open('README.rst', mode='r') as fd:
+    long_description = fd.read()
+
+from rheostatic import __version__
+
+setup(
+    name='Rheostatic',
+    version=__version__,
+    url='https://github.com/waylan/rheostatic',
+    description='A Static File Server with options.',
+    long_description=long_description,
+    author='Waylan Limberg',
+    author_email='waylan.limberg@icloud.com',
+    license='MIT License',
+    packages=['rheostatic', 'rheostatic.tests'],
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Internet :: WWW/HTTP :: HTTP Servers',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+        'Topic :: Utilities',
+        'Intended Audience :: Developers'
+    ]
+)
