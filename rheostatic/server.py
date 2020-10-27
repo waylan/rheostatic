66ee9bf145ca1d30aa752cde77b88c7bb4b78bc0
Waylan Limberg <waylan.limberg@icloud.com>
2020-10-27 10:16:03 -0400

    Update to PY36+

--- rheostatic/server.py
+++ rheostatic/server.py
@@ -24,7 +24,6 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
-from __future__ import print_function
 from wsgiref.validate import validator
 from wsgiref.simple_server import make_server
 
